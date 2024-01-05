e4326fe7d294b1b46ccc4811e5bb0a8a71e52530
Nadav Peled <nadav.peledlevin@gmail.com>
2024-01-05 09:29:51 +0200

    finish Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,67 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+//Algorithm:
+// 1. set counter to 8191
+// 2. loop:
+//      if key is pressed: black screen
+//      else: white screen
+// 3. set counter to the next pixels 'word'
+// 4. if RAM[counter]=-1: we finish changing the entire screen
+//    else: return to LOOP
+
+// Pseudo-code:
+// counter = 8191
+// loop:
+//      if KBD = -1: black screen
+//      else: white screen
+//      counter = counter - 1
+//      if counter = 0: return to loop
+//      else: finish
+
+(START)
+    @8191
+    D=A
+    @counter
+    M=D
+
+(LOOP)
+    @KBD
+    D=M
+    // black screen if key is pressed
+    @BLACK
+    D;JNE
+    // else white screen
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    @counter
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @NEXT
+    0;JMP
+
+(WHITE)
+    @counter
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @NEXT
+    0;JMP
+
+(NEXT)
+    // set counter to the next pixels 'word'
+    @counter
+    M=M-1
+    // if RAM[counter]=-1: we finish changing the entire screen
+    @counter
+    D=M+1
+    @START
+    D;JEQ
+    // else: return to LOOP
+    @LOOP
+    0;JMP
